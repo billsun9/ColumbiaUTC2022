8769eadcc1873a20d7512a66c4d073742daef6ee
bill <billsun9@gmail.com>
2022-04-01 12:43:06 -0400

    whats going on

--- case1/model.py
+++ case1/model.py
@@ -37,4 +37,15 @@ plt.plot(list(range(252)), r, label="rain")
 plt.legend()
 plt.show()
 # %%
+X = rain.to_numpy()[:,1].reshape(-1,1)
+X = np.append(X, np.array(list(range(1,13))).reshape(-1,1), axis=1)
+Y = prices.to_numpy()[:,1].reshape(12,21)
+# %%
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 # high rain => harder for loggers => higher spot prices
+model = LinearRegression()
+model.fit(X, Y)
+# %%
+pred = model.predict(X[11].reshape(1,-1))
+print("mse: %s" % str(mean_squared_error(Y[11].reshape(1,-1), pred)))
